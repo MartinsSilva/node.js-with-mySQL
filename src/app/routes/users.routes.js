0f6b8d5c8d87de18f21b8b9b49acb2ea1503370a
GentilPinto <gentil.junior.gp@gmail.com>
2020-10-13 22:24:08 +0100

    add: users routes file

---
+++ src/app/routes/users.routes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/users.controller');
+
+
+router.get('/', usersController.getUsers);
+router.get('/:id', usersController.getUser);
+
+module.exports = router;
