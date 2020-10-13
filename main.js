581a6c498d75288b696f4657995b17f3b1899c5c
GentilPinto <gentil.junior.gp@gmail.com>
2020-10-13 22:29:32 +0100

    delete: unnecessary files

--- main.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routers = require('./routes');
-require('dotenv').config();
-
-const app = express();
-app.use(routers);
-
-app.listen(process.env.APP_PORT);
